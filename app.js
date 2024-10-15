632ac0b2482e6207d4c9d15aea92cbe4b1a5236b
Staasie <ana.stay.stay@gmail.com>
2024-10-15 14:04:12 -0700

    lab 7 site

--- app.js
+++ app.js
@@ -1,23 +1,60 @@
-'use strict';
+'use strict'
 
-let time = Number(prompt('What hour is it? (0-23)?'));
-let message;
-let username;
-let favoritesong; 
-if (time < 0 || time > 23) { 
-    message = 'That hour does not exist!';
-} else if (time <= 11) {
-    message = 'Good Morning!';
-} else if (time <= 18) {
-    message = 'Good Afternoon!';
-} else {
-    message = 'Good Evening!';
+function yourName() {
+    let name = prompt('What is your name?');
+    return document.write(name);
 }
 
-username = prompt('What is your name?');
-console.log('Hello, ' + username);
+function favoriteSong() {
+    let song = prompt ('What\'s your favorite song?');
+    return document.write(song)
+}
 
-favoritesong = prompt('What is your favorite song?');
-console.log('Great choice, who doesn\'t love ' + favoritesong + '?');
+function timeOfDay() {
+    let time = prompt('What hour is it? (0-23)');
+    let message = '';
 
-document.write(message + ' Hello ' + username + '! Great choice, who doesn\'t love ' + favoritesong + '?');
+    if (time < 0 || time > 23) { 
+        message = 'That hour does not exist!';
+    } else if (time <= 11) {
+        message = 'Good Morning!';
+    } else if (time <= 18) {
+        message = 'Good Afternoon!';
+    } else {
+        message = 'Good Evening!';
+    }
+
+    return message;
+}
+
+// let username = yourName(); 
+// let timeMessage = timeOfDay();
+
+// let favoritesong = prompt('What is your favorite song?');
+
+let finalMessage = `${timeMessage} Hello ${username}! Great choice, who doesn\'t love ${favoritesong}?`;
+
+document.body.innerHTML = finalMessage;
+
+
+// 'use strict';
+
+// let time = prompt('What hour is it? (0-23)');
+// let message;
+// let username = prompt('What is your name?');
+// let favoritesong = prompt('What is your favorite song?');
+// if (time < 0 || time > 23) { 
+//     message = 'That hour does not exist!';
+// } else if (time <= 11) {
+//     message = 'Good Morning!';
+// } else if (time <= 18) {
+//     message = 'Good Afternoon!';
+// } else {
+//     message = 'Good Evening!';
+// }
+
+// console.log('Hello, ' + username);
+
+// console.log('Great choice, who doesn\'t love ' + favoritesong + '?');
+
+// document.write(message + ' Hello ' + username + '! Great choice, who doesn\'t love ' + favoritesong + '?');
