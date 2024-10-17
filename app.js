1bbf7aec83aaf5d97e7dc3e0f8b74077b26ddb85
Staasie <ana.stay.stay@gmail.com>
2024-10-17 14:49:48 -0700

    album tings

--- app.js
+++ app.js
@@ -27,14 +27,62 @@ function timeOfDay() {
     return message;
 }
 
+function displayRating() {
+    let output = '';
+    let rating = prompt ('scale of 1-5, how many stars would you rate my website?');
+    for(let i = 0; i < rating; i++){
+        output += "<img class= 'star' src='https://www.citypng.com/public/uploads/preview/hd-glowing-neon-yellow-star-transparent-png-7017516945411456fzdnwlob3.png' width='50' height='50'/>";
+    }
+    return document.write(output);
+}
+ 
+function rateMyPage() {
+    let answer = prompt('rate my webpage');
+    let output = '';
+
+    if (answer >= 4) { 
+        message = 'so happy you enjoy the album store webpage!';
+    } else if (answer <= 3) {
+        message = 'we/ll do better';
+    } else if (answer < 3) {
+        message = 'well that is no fun!';
+    }
+
+    return document.write(output);
+}
+
+
+
+
+
+
+
+
+// 'use strict'
+
+// function yourName() {
+//     let name = prompt('What is your name?');
+//     return document.write(name);
+// }
+
+// function guess answer() {
+//     let answer;
+
+//     while (answer !=9) {
+//         alert('Try Again!');
+// //     } else 
+// }
+
+
+
 // let username = yourName(); 
 // let timeMessage = timeOfDay();
 
 // let favoritesong = prompt('What is your favorite song?');
 
-let finalMessage = `${timeMessage} Hello ${username}! Great choice, who doesn\'t love ${favoritesong}?`;
+// let finalMessage = `${timeMessage} Hello ${username}! Great choice, who doesn\'t love ${favoritesong}?`;
 
-document.body.innerHTML = finalMessage;
+// document.body.innerHTML = finalMessage;
 
 
 // 'use strict';
@@ -58,3 +106,5 @@ document.body.innerHTML = finalMessage;
 // console.log('Great choice, who doesn\'t love ' + favoritesong + '?');
 
 // document.write(message + ' Hello ' + username + '! Great choice, who doesn\'t love ' + favoritesong + '?');
+
+
